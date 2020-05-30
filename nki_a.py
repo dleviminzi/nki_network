c59436c6293eb829d6d0627102ad19e571f4491a
pauladkisson <paul.wesley.adkisson@gmail.com>
2020-05-30 15:44:29 -0700

    Updated .mlx file to account for different thresholds, .py file to streamline subject ids, and corresponding mlx_graphs, p_vals.csv, and o.csv

--- nki_a.py
+++ nki_a.py
@@ -3,12 +3,12 @@ import networkx as nx
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.metrics import r2_score
-import os
+import os 
 
 gpickle_list = os.listdir('./gpickle_data')
 
 csv = open("o.csv", "w")
-csv.write('session_id,session_number,threshold,density, avg_clst, avg_deg, r2\n')
+csv.write('subject_id,session_number,threshold,density, avg_clst, avg_deg, r2\n')
 
 # this value can be specified to cut off edges based on their weights
 # this indicates what percent and up you want
@@ -20,7 +20,7 @@ for thresh in thresholds:
         # compute density and avg clustering of nodes
         density = nx.density(G)
         avg_clustering = nx.average_clustering(G)
-        # print('smlwrld: {}\n'.format(nx.sigma(G)))
+        #print('smlwrld: {}\n'.format(nx.sigma(G)))
 
         # graphing topological hierarchy (Negative correlation btwn degree
         # and clustering coefficient)
@@ -36,19 +36,19 @@ for thresh in thresholds:
             dg_values.append(i[1])
 
         avg_deg = sum(dg_values)/len(dg_values)
-        # Code to extract subject id and session number
-        subject_id = gp.split("_")[0]
+        #Code to extract subject id and session number
+        subject_id = gp.split("_")[0].split("-")[1]
         session_number = gp.split("ses-")[1].split("_")[0]
         # creating est fit line
         best_fit = np.poly1d(np.polyfit(dg_values, cn_values, 1))
         r2 = r2_score(cn_values, best_fit(dg_values))
 
         # saving to csv
-        csv.write('{},{},{},{},{},{},{}\n'.format(subject_id, session_number, thresh, density, avg_clustering, avg_deg, r2))
+        csv.write('{},{},{},{},{},{},{}\n'.format(subject_id,session_number, thresh, density, avg_clustering, avg_deg, r2))
 
 
-        ''' generating output plot
-        Zlt.clf()
+        ''' generating output plot 
+        plt.clf()
         plt.ylim(0.25, 1.10)
         plt.scatter(dg_values, cn_values)
         plt.plot(np.unique(dg_values), best_fit(np.unique(dg_values)))

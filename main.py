169a0b83623b84df5b2fce31ada793459c629136
pauladkisson <paul.wesley.adkisson@gmail.com>
2020-06-07 13:34:04 -0700

    Added Scatterplots that plot mean network stat values against threshold

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from analyzeNKI import analyzeNKI
 from degDist import degDist
 
-thresholds = [10,20,30,40,50,60,70,80,90]
+thresholds = [1,10,20,30,40,50,60,70,80,90,92.5,95,97.5]
 
 analyzeNKI(thresholds, directed=False, gpickle_dir='./gpickle_data/')
 degDist(thresholds, gpickle_dir='./gpickle_data/')
